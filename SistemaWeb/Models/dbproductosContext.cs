9314a6dbc1c3fe9fd1d576b247344a392548f1ed
Luis Bragado <luis.bragado.mx@gmail.com>
2020-03-28 10:13:44 -0600

    Inicializamos el proyecto

---
+++ SistemaWeb/Models/dbproductosContext.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace SistemaWeb.Models
+{
+    public partial class dbproductosContext : DbContext
+    {
+        public dbproductosContext()
+        {
+        }
+
+        public dbproductosContext(DbContextOptions<dbproductosContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Categoria> Categoria { get; set; }
+        public virtual DbSet<Producto> Producto { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=dbproductos;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categoria>(entity =>
+            {
+                entity.HasKey(e => e.Idcategoria)
+                    .HasName("PK__categori__140587C76288009F");
+
+                entity.ToTable("categoria");
+
+                entity.HasIndex(e => e.Nombre)
+                    .HasName("UQ__categori__72AFBCC6B49DE3A0")
+                    .IsUnique();
+
+                entity.Property(e => e.Idcategoria).HasColumnName("idcategoria");
+
+                entity.Property(e => e.Descripcion)
+                    .HasColumnName("descripcion")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Estado)
+                    .HasColumnName("estado")
+                    .HasDefaultValueSql("((1))");
+
+                entity.Property(e => e.Nombre)
+                    .IsRequired()
+                    .HasColumnName("nombre")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Producto>(entity =>
+            {
+                entity.HasKey(e => e.Idproducto)
+                    .HasName("PK__producto__DC53BE3C8D37D870");
+
+                entity.ToTable("producto");
+
+                entity.HasIndex(e => e.Nombre)
+                    .HasName("UQ__producto__72AFBCC6A741D49C")
+                    .IsUnique();
+
+                entity.Property(e => e.Idproducto).HasColumnName("idproducto");
+
+                entity.Property(e => e.Codigo)
+                    .IsRequired()
+                    .HasColumnName("codigo")
+                    .HasMaxLength(64)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Descripcion)
+                    .HasColumnName("descripcion")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Estado)
+                    .HasColumnName("estado")
+                    .HasDefaultValueSql("((1))");
+
+                entity.Property(e => e.Idcategoria).HasColumnName("idcategoria");
+
+                entity.Property(e => e.Nombre)
+                    .IsRequired()
+                    .HasColumnName("nombre")
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.PrecioVenta)
+                    .HasColumnName("precio_venta")
+                    .HasColumnType("decimal(11, 2)");
+
+                entity.Property(e => e.Stock).HasColumnName("stock");
+
+                entity.HasOne(d => d.IdcategoriaNavigation)
+                    .WithMany(p => p.Producto)
+                    .HasForeignKey(d => d.Idcategoria)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__producto__idcate__29572725");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
